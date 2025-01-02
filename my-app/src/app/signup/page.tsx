11c71eacf21cc0025906a091ea4bf2f480949436
Thibaud <thibaudfresnay.pro@gmail.com>
2025-01-02 14:18:14 +0100

    in signu page the component has been updated with useState function and signup input has been created

--- my-app/src/app/signup/page.tsx
+++ my-app/src/app/signup/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function SignupPage() {
-  return (
-    <div className="flex">
-      <h1 className="text-center text-2xl">Signup</h1>
-    </div>
-  );
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: "",
+    });
+    const onSignUp = async () => {
+
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Signup</h1>
+            <hr />
+            <label htmlFor="username">username</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                id="username"
+                type="text"
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+                placeholder="username"
+            />
+        </div>
+    );
 }
