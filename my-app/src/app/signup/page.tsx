0862c44d2dfeedfa1ce4f529fa1eb7f8bcd81cc2
Thibaud <thibaudfresnay.pro@gmail.com>
2025-01-03 10:40:04 +0100

    signup page.tsx updated after pasting the login's code page  in signup page as they're kind of sames

--- my-app/src/app/signup/page.tsx
+++ my-app/src/app/signup/page.tsx
@@ -4,29 +4,38 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { axios } from "axios";
 
-export default function SignupPage() {
+export default function LoginPage() {
     const [user, setUser] = React.useState({
         email: "",
-        password: "",
-        username: "",
+        password: ""
     });
-    const onSignUp = async () => {
+    const onLogin = async () => {
 
     }
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Signup</h1>
+            <h1>Login</h1>
             <hr />
-            <label htmlFor="username">username</label>
             <input
                 className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-                id="username"
+                id="email"
                 type="text"
-                value={user.username}
-                onChange={(e) => setUser({ ...user, username: e.target.value })}
-                placeholder="username"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                placeholder="email"
             />
+            <label htmlFor="password">password</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                placeholder="password"
+            />
+            <button className="p-2 bg-blue-500 text-white rounded-lg" onClick={onLogin}>Login</button>
+            <Link href="/login">Visit the signup page</Link>
         </div>
     );
 }
